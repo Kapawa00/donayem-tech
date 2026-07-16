da674eabc9e7ab331d625062a773c8f8536b5a21
Morange-tech <baesara803@gmail.com>
2026-07-16 11:53:38 +0100

    update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,9 +16,31 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
+    /*
+    | FRONTEND_URL ne couvre qu'une seule origine (ex: https://donayemtech.com).
+    | On dérive automatiquement la variante www./non-www. pour éviter les 403 CORS
+    | quand le site est servi sous les deux formes (cas DONAYEM TECH en prod).
+    */
+    'allowed_origins' => (function () {
+        $frontendUrl = env('FRONTEND_URL', 'http://localhost:3000');
+        $origins = [$frontendUrl];
+
+        $parts = parse_url($frontendUrl);
+        if (isset($parts['host'])) {
+            $altHost = str_starts_with($parts['host'], 'www.')
+                ? substr($parts['host'], 4)
+                : 'www.'.$parts['host'];
+
+            $altUrl = $parts['scheme'].'://'.$altHost;
+            if (isset($parts['port'])) {
+                $altUrl .= ':'.$parts['port'];
+            }
+
+            $origins[] = $altUrl;
+        }
+
+        return $origins;
+    })(),
 
     'allowed_origins_patterns' => [],
 
